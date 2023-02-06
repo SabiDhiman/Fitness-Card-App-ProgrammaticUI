0395dc85582258eec6dcfdd75d51c2546615c09a
SabiDhiman <sabi98@live.co.uk>
2023-02-06 21:24:48 +0000

    added image ands constraints

--- Fitness-Programmatic2/CWButtons.swift
+++ Fitness-Programmatic2/CWButtons.swift
@@ -17,9 +17,15 @@ class CWButtons: UIButton {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    //create custom init for buttons
     
+    init(backgroundColor: UIColor, title: String){
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+    }
     
-    
+    //start with configure function
     func configure(){
         layer.cornerRadius = 8
         titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
--- Fitness-Programmatic2/CardSelectionVC.swift
+++ Fitness-Programmatic2/CardSelectionVC.swift
@@ -8,13 +8,44 @@
 import UIKit
 
 class CardSelectionVC: UIViewController {
+    
+    let cardImageView   = UIImageView()
+    
+    let stopButton      = CWButtons(backgroundColor: .systemRed, title: "Stop")
+    let restartButton   = CWButtons(backgroundColor: .systemGreen, title: "Restart")
+    let rulesButton     = CWButtons(backgroundColor: .systemBlue, title: "Rules")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
-
+        view.backgroundColor = .systemBackground
+        configureUI()
     }
     
-
-
+    func configureUI(){
+        configureCardImageView()
+    }
+    func configureCardImageView(){
+        view.addSubview(cardImageView)
+        cardImageView.translatesAutoresizingMaskIntoConstraints = false
+        cardImageView.image = UIImage(named: "2_of_clubs")
+        
+        NSLayoutConstraint.activate([
+            cardImageView.widthAnchor.constraint(equalToConstant: 250),
+            cardImageView.heightAnchor.constraint(equalToConstant: 350),
+            cardImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            cardImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -75)
+        
+        
+        ])
+    }
+    func configureStopButton(){
+        
+    }
+    func configureRestartButton(){
+        
+    }
+    func configureRulesButton(){
+        
+        
+    }
 }
